b8be7a19eb903bb92241e339f83059bad3dc110c
ideaki <ikudeve@gmail.com>
2014-12-09 23:49:09 +0900

    Canvas下限定に修正

--- Ika.Controls/Behaviors/ControlMoveBehavior.cs
+++ Ika.Controls/Behaviors/ControlMoveBehavior.cs
@@ -26,14 +26,6 @@ namespace Ika.Controls.Behaviors
         public void Attach(DependencyObject associatedObject)
         {
             AssociatedObject = associatedObject;
-            //SetBinding(
-            //    DataContextProperty,
-            //    new Binding
-            //    {
-            //        Path = new PropertyPath("DataContext"),
-            //        Source = AssociatedObject,
-            //        Mode = BindingMode.TwoWay
-            //    });
             var fe = AssociatedObject as FrameworkElement;
             fe.Loaded += fe_Loaded;
         }
@@ -65,34 +57,20 @@ namespace Ika.Controls.Behaviors
 
         private void Control_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-            var fe = e.OriginalSource as FrameworkElement;
-            var canvas = fe.Parent as FrameworkElement;
+            var fe = sender as FrameworkElement;
+            var canvas = ((FrameworkElement)fe).Parent as FrameworkElement;
             var x = Canvas.GetLeft(fe) + e.Delta.Translation.X;
             var y = Canvas.GetTop(fe) + e.Delta.Translation.Y;
 
-            //Debug.WriteLine(string.Join(" : ", canvas.ActualHeight, canvas.ActualWidth, x, y, _zindex));
-            //x = x <= 0 ? 0 : ActualWidth + x >= canvas.ActualWidth ? canvas.ActualWidth - ActualWidth : x;
-            //y = y <= 0 ? 0 : ActualHeight + y >= canvas.ActualHeight ? canvas.ActualHeight - ActualHeight : y;
-            //Canvas.SetLeft(fe, x);
-            //Canvas.SetTop(fe, y);
-            var transform = (fe.RenderTransform = (fe.RenderTransform as CompositeTransform) ?? new CompositeTransform()) as CompositeTransform;
-            //transform.TranslateX += x;
-            //transform.TranslateY += y;
-            transform.TranslateX += e.Delta.Translation.X;
-            transform.TranslateY += e.Delta.Translation.Y;
-            //VisualStateManager.GoToState(fe, "dragging", true);
+            x = x <= 0 ? 0 : fe.ActualWidth + x >= canvas.ActualWidth ? canvas.ActualWidth - fe.ActualWidth : x;
+            y = y <= 0 ? 0 : fe.ActualHeight + y >= canvas.ActualHeight ? canvas.ActualHeight - fe.ActualHeight : y;
+            Canvas.SetLeft(fe, x);
+            Canvas.SetTop(fe, y);
+            //VisualStateManager.GoToState(this, "dragging", true);
             Canvas.SetZIndex(fe, zindex++);
-            //var _RotateTransform = (fe.RenderTransform = (fe.RenderTransform as RotateTransform) ?? new RotateTransform()) as RotateTransform;
-            //_RotateTransform.Angle += e.Delta.Rotation;
+            var _RotateTransform = (fe.RenderTransform = (fe.RenderTransform as RotateTransform) ?? new RotateTransform()) as RotateTransform;
+            _RotateTransform.Angle += e.Delta.Rotation;
             //Debug.WriteLine(_RotateTransform.Angle);
-            return;
-
-            var element = sender as FrameworkElement;
-
-            if (transform == null)
-                element.RenderTransform = (transform = new CompositeTransform());
-            transform.TranslateX += e.Delta.Translation.X;
-            transform.TranslateY += e.Delta.Translation.Y;
         }
 
         private void Control_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
@@ -101,25 +79,6 @@ namespace Ika.Controls.Behaviors
             var _RotateTransform = (fe.RenderTransform = (fe.RenderTransform as RotateTransform) ?? new RotateTransform()) as RotateTransform;
             _RotateTransform.Angle = ((int)_RotateTransform.Angle) / 30 * 30;
             //VisualStateManager.GoToState(fe, "normal", true);
-            return;
-            //var _Rectangle = sender as Windows.UI.Xaml.Shapes.Rectangle;
-            var _Rectangle = fe;
-            _Rectangle.RenderTransform = null;
-
-            var _Column = System.Convert.ToInt16(_Rectangle.GetValue(Grid.ColumnProperty));
-            if (_Column <= 0 && e.Cumulative.Translation.X > _Rectangle.RenderSize.Width * .5)
-                _Rectangle.SetValue(Grid.ColumnProperty, 1);
-            else if (_Column == 1 && e.Cumulative.Translation.X < _Rectangle.RenderSize.Width * -.5)
-                _Rectangle.SetValue(Grid.ColumnProperty, 0);
-
-            var _Row = System.Convert.ToInt16(_Rectangle.GetValue(Grid.RowProperty));
-            if (_Row <= 0 && e.Cumulative.Translation.Y > _Rectangle.RenderSize.Height * .5)
-                _Rectangle.SetValue(Grid.RowProperty, 1);
-            else if (_Row == 1 && e.Cumulative.Translation.Y < _Rectangle.RenderSize.Height * -.5)
-                _Rectangle.SetValue(Grid.RowProperty, 0);
-            //Debug.WriteLine(string.Join(" : ", _Column, _Row));
-            //Debug.WriteLine(string.Join(" : ", e.Cumulative.Translation.X, e.Cumulative.Translation.Y));
-            //Canvas.SetZIndex(fe, 0);
         }
 
         private void Control_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
