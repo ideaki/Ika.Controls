0634333c275acf44a074873ffabd6ae83ff4f23c
ideaki <ikudeve@gmail.com>
2014-12-14 04:43:09 +0900

    ずりずり追加　引っ張る幅についての課題あり

--- Ika.Controls/PullToRefreshPanel/PullToRefreshPanel.cs
+++ Ika.Controls/PullToRefreshPanel/PullToRefreshPanel.cs
@@ -21,7 +21,7 @@ namespace Ika.Controls
         public PullToRefreshPanel()
         {
             DefaultStyleKey = typeof(PullToRefreshPanel);
-            FontSize = 36;
+            FontSize = 28;
         }
 
 
@@ -49,6 +49,22 @@ namespace Ika.Controls
                                         typeof(PullToRefreshPanel),
                                         new PropertyMetadata("引っ張って"));
 
+
+
+        public double PullRange
+        {
+            get { return (double)GetValue(PullRangeProperty); }
+            set { SetValue(PullRangeProperty, value); }
+        }
+
+        public static readonly DependencyProperty PullRangeProperty =
+            DependencyProperty.Register("PullRange",
+                                        typeof(double),
+                                        typeof(PullToRefreshPanel),
+                                        new PropertyMetadata(200.0));
+
+
+
         public event EventHandler PullToRefresh;
 
         void UpdateStates(bool useTransitions)
@@ -70,7 +86,6 @@ namespace Ika.Controls
             ScrollViewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
             ScrollViewer.SizeChanged += ScrollViewer_SizeChanged;
             ScrollViewer.ViewChanged += ScrollViewer_ViewChanged;
-            UpdateStates(false);
             base.OnApplyTemplate();
         }
 
@@ -84,20 +99,33 @@ namespace Ika.Controls
             content.SetValue(WidthProperty, ScrollViewer.ActualWidth);
         }
 
-        async void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
+
             UpdateStates(true);
+
             if (ScrollViewer.VerticalOffset != 0.0)
                 isPullRefresh = true;
 
+
+            var Element = GetTemplateChild("StackPanel") as UIElement;
+            var Transform = Element.RenderTransform as CompositeTransform;
+            if (Transform == null)
+                Transform = new CompositeTransform();
+
+            Transform.TranslateY = (ScrollViewer.VerticalOffset - PullRange) * 0.8;
+
+            Element.RenderTransform = Transform;
+
             var grid = GetTemplateChild("Grid") as Grid;
+
             if (!e.IsIntermediate)
             {
                 if (ScrollViewer.VerticalOffset == 0.0 && isPullRefresh)
                 {
                     OnPullToRefresh(new EventArgs());
 
-                    await Task.Delay(100);
+                    //await Task.Delay(50);
                 }
                 isPullRefresh = false;
                 ScrollViewer.ChangeView(null, grid.ActualHeight, null);
