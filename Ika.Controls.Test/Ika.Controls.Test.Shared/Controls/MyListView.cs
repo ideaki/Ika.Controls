ce0e155e3cc23c00febf3a71faa1a14cd091b558
ideaki <ikudeve@gmail.com>
2014-12-13 16:03:14 +0900

    テストの追加

---
+++ Ika.Controls.Test/Ika.Controls.Test.Shared/Controls/MyListView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Ika.Controls.Test
+{
+    public class MyListView : ListView
+    {
+        protected override void OnApplyTemplate()
+        {
+            ItemsSource = Enumerable.Range(1, 500);
+            base.OnApplyTemplate();
+        }
+
+        /// <summary>
+        /// アイテムに色と高さを設定
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="item"></param>
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            int index = IndexFromContainer(element);
+            //if (index % 2 == 0)
+            //{
+            //    element.SetValue(BackgroundProperty, new SolidColorBrush(Color.FromArgb((byte)0, (byte)(index/256/256),(byte)(index/256),(byte)index)));
+            //}
+            //else
+            //{
+            //    element.SetValue(BackgroundProperty, new SolidColorBrush(Colors.Black));
+            //}
+            var r = Math.Min(255, Math.Max(0, 255 - index));
+            var g = Math.Min(255, Math.Max(0, 510 - index));
+            var b = Math.Min(255, index);
+            element.SetValue(BackgroundProperty, new SolidColorBrush(Color.FromArgb((byte)255, (byte)r, (byte)g, (byte)b)));
+            //element.SetValue(HeightProperty, index + 10);
+            base.PrepareContainerForItemOverride(element, item);
+        }
+
+    }
+}
